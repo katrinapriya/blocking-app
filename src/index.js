aee7b15cb30d7beded8b7e01088a4fc981330b76
michellefan <fan.michellec@gmail.com>
2018-03-18 18:56:58 -0700

    made responsive color box

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  state = {value:this.props.value,
-  };
+  state = {value:this.props.value};
   render() {
     return (
-      <button className="square" onClick={() =>
-        this.props.onClick()}>{this.props.value}
+      <button className="square" onClick={() => {this.props.onClick()}}>{this.props.value}
       </button>
     );
   }
@@ -16,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   state = {
-      squares: Array(9).fill(null),
+      squares: Array(4).fill(null),
   };
 
 
@@ -25,11 +23,18 @@ class Board extends React.Component {
     squares[i] = 'X';
     this.setState({squares: squares});
   }
-  
+
   renderSquare(i) {
-    return (<Square value={this.state.squares[i]}
-    onClick = {() => this.handleClick(i)}
-    />);
+    return (
+      <div>
+        <Square value={this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+        />
+        <div className="notes">
+        Hello
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -40,23 +45,21 @@ class Board extends React.Component {
         <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
+        </div>
+        <div className="board-row">
           {this.renderSquare(1)}
+        </div>
+        <div className="board-row">
           {this.renderSquare(2)}
         </div>
         <div className="board-row">
           {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
         </div>
       </div>
     );
   }
 }
+
 
 class Game extends React.Component {
   render() {
